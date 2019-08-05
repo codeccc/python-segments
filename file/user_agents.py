8270947e5ada35b57f31ca0420cb6532b256825b
wangbo <wb1276831936@163.com>
2019-08-05 15:09:15 +0800

    添加user-agents用户表示片段

--- file/user_agents.py
+++ file/user_agents.py
@@ -1,39 +1,3 @@
-# python-segments
-日常编写/收集一些通用的代码片段
-
-
-
-#### 1. 遍历文件夹得到目录下所有文件 [file_iter.py](https://github.com/codeccc/python-segments/file/file_iter.py)
-
-代码如下: `your_path`  为你要扫描的文件夹路径. 
-
-```python
-# encoding=utf-8
-
-import os
-from prettyprinter import pprint
-
-your_path=r'C:\Users\Administrator\Pictures' # 这里定义你要遍历的路径
-
-def read_file(f_path):
-    if os.path.isdir(f_path): #判断是否为文件夹
-        f_list = os.listdir(f_path) # 获得目录下所有文件
-        for file in f_list:
-            read_file(f_path + '\\' + file) # 递归再进行读取
-    else:
-        with open(f_path) as f:
-            pprint(f.name) # 打印文件路径
-
-#开始遍历读取
-read_file(your_path)
-
-```
-
-#### 2. 常见的UserAgent大全  [user_agents.py](https://github.com/codeccc/python-segments/file/fuser_agents.py)
-
-代码如下:
-
-```python
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
@@ -76,5 +40,4 @@ mobileUserAgent = {
     "UCOpenwave":"User-Agent: Openwave/ UCWEB7.0.2.37/28/999",
     "UC Opera":"User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; ) Opera/UCWEB7.0.2.37/28/999"
 }
-```
 
